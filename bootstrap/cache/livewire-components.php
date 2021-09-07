f3567bbee053449e6d7a85ea7c6bdece26871bb7
sehersaygili <77735385+sehersaygili@users.noreply.github.com>
2021-09-08 02:13:10 +0300

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'admin.admin-dashboard-component' => 'App\\Http\\Livewire\\Admin\\AdminDashboardComponent',
+  'cart-component' => 'App\\Http\\Livewire\\CartComponent',
+  'checkout-component' => 'App\\Http\\Livewire\\CheckoutComponent',
+  'details-component' => 'App\\Http\\Livewire\\DetailsComponent',
+  'home-component' => 'App\\Http\\Livewire\\HomeComponent',
+  'product-detail-component' => 'App\\Http\\Livewire\\ProductDetailComponent',
+  'productlist-component' => 'App\\Http\\Livewire\\ProductlistComponent',
+  'user.user-dashboard-component' => 'App\\Http\\Livewire\\User\\UserDashboardComponent',
+);
